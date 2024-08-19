ae8df965903fce6ab8335139b6d4f0dc7eaed602
Tesmi-Devlop <sashdfr@gmail.com>
2024-08-19 21:20:34 +1000

    fix: evn loading

--- HypercubeBot/Program.cs
+++ HypercubeBot/Program.cs
@@ -6,9 +6,27 @@ namespace HypercubeBot;
 
 public class Program
 {
+    public static void LoadEvn(string filePath)
+    {
+        if (!File.Exists(filePath))
+            return;
+
+        foreach (var line in File.ReadAllLines(filePath))
+        {
+            var parts = line.Split(
+                '=',
+                StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length != 2)
+                continue;
+
+            Environment.SetEnvironmentVariable(parts[0], parts[1]);
+        }
+    }
+    
     public static async Task Main(string[] args)
     {
-        DotEnv.Load();
+        LoadEvn(".env");
         DependencyManager.InitThread();
         
         ServiceManager.CreateAll();
