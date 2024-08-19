c32d3146b1c74b28bd867048db61a22571977746
Tesmi-Devlop <sashdfr@gmail.com>
2024-08-20 00:16:09 +1000

    fix: return DotEnv.Load

--- HypercubeBot/Program.cs
+++ HypercubeBot/Program.cs
@@ -6,27 +6,9 @@ namespace HypercubeBot;
 
 public class Program
 {
-    public static void LoadEvn(string filePath)
-    {
-        if (!File.Exists(filePath))
-            return;
-
-        foreach (var line in File.ReadAllLines(filePath))
-        {
-            var parts = line.Split(
-                '=',
-                StringSplitOptions.RemoveEmptyEntries);
-
-            if (parts.Length != 2)
-                continue;
-
-            Environment.SetEnvironmentVariable(parts[0], parts[1]);
-        }
-    }
-    
     public static async Task Main(string[] args)
     {
-        LoadEvn(".env");
+        DotEnv.Load();
         DependencyManager.InitThread();
         
         ServiceManager.CreateAll();
