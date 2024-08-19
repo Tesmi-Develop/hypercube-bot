0f053ea918320de29e7b8887100bfbbfcef7ae9d
Tesmi-Devlop <sashdfr@gmail.com>
2024-08-20 01:51:56 +1000

    ref: DiscordUserSchema

--- HypercubeBot/Schemas/DiscordUserSchema.cs
+++ HypercubeBot/Schemas/DiscordUserSchema.cs
@@ -1,15 +1,13 @@
 ﻿using JetBrains.Annotations;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
 
 namespace HypercubeBot.Schemas;
 
 [Serializable, Collection("Users"), PublicAPI]
 public sealed class DiscordUserSchema : Schema
 {
-    public string AccessToken { get; set; }
-    public string RefreshToken { get; set; }
-    public DateTime TokenExpiration { get; set; }
+    public string AccessToken { get; set; } = "";
+    public string RefreshToken { get; set; } = "";
+    public DateTime TokenExpiration { get; set; } = DateTime.Now;
 
     public DiscordUserSchema(string id) : base(id)
     {
